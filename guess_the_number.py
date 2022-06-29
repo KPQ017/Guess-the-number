375a9af747db0b3f19faf55e8dc8299fbf119e44
sakur <reina.u.u.23707@akane.waseda.jp>
2022-06-29 12:09:50 +0900

    programing ->  guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -4,3 +4,20 @@ ans=random.randint(100,999)
 max_count=10
 
 print('三桁の数字を一つ選んだよ．ヒントを頼りに',max_count,'回以内に当てよう！')
+
+for i in range(1,max_count+1):
+    print(i,'回目')
+    num=int(input())
+
+    if num==ans:
+        print('正解')
+        break
+
+    elif num > ans:
+        print('小')
+    else:
+        print('大')
+
+else:
+
+    print('正解は',ans)
